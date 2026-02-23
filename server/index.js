c2a1333cc96a690d9122af60d2e41daef85d967e
Sergey Vishnyakov <sergeynew37@gmail.com>
2026-02-23 14:33:14 +1000

    Add db connect log

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // Render will provide the PORT in produc
 app.use(cors());
 app.use(express.json()); // This allows us to parse JSON bodies in POST requests
 const path = require('path');
-
+console.log("Checking MONGO_URI:", process.env.MONGO_URI ? "Found it!" : "Still undefined");
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB Atlas!"))
   .catch(err => console.error("Could not connect:", err));
